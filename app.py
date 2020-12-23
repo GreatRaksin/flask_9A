d961196a8dffd958a8acdfc708b39131018a1a37
demidraksin <greatraksin@icloud.com>
2020-12-23 10:25:36 +0300

    Adding styles for images

--- app.py
+++ app.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    images = [
+        'https://images.unsplash.com/photo-1608592077365-c6399182e63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1608636142902-9bfb65933f52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1608696999843-e1f3f98975a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80',
+        'https://images.unsplash.com/photo-1511742347168-314fb2a3efb3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1368&q=80',
+        'https://images.unsplash.com/photo-1608683110609-c14f250e3e4b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1608679152045-b827c386d312?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    ]
+    return render_template('index.html', im=images)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
